6529cf902c15badd99b20e0c6a0b1d286d0cb37e
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-05-04 18:16:46 -0500

    added React Router template to App.js, Home was created and Navbar has been imported. Navlinks has a map on it, and the array will be changed at a later time, but it is displaying as it should. The basic outline of this project has been created with necessary files

---
+++ src/Component/Global/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+
+    const navLinks = [
+        {
+            linkName: "home",
+            href: '/',
+            className: "navbar-link",
+        },
+        {
+            linkName: "About",
+            href: '/about',
+            className: "navbar-link",
+        },
+        {
+            linkName: "Contact Us",
+            href: '/contact-us',
+            className: "navbar-link",
+        },
+
+    ]
+
+  return (
+    <nav>
+      {navLinks.map((item, index) => (
+        <Link key={index} className={item.className} to={item.href}>
+            {item.linkName}
+        </Link>
+      ))}
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/Component/Home/ModelType.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ModelType() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ModelType
