9a4d53913d051e73d5ceae76ff5e2dfcc6b0f093
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-05-06 15:57:42 -0500

    Added to the theme, navbar has more data and some CSS files have been created.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dreamsicle': '#FFA17F',
+        'vibrant-orange': '#FF851B',
+        'soft-peach': '#FFD1B2',
+        'cream-white': '#FFF3E4',
+        'warm-grey': '#D3D3D3',
+        'dark-charcoal': '#333333',
+        'sky-blue': '#00A4CC',
+      }
+    },
   },
   plugins: [],
 }
+
