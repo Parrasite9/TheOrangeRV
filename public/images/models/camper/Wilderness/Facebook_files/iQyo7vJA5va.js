ee4828292a30f1ad2c79d859dd8aa58b1ca6ca9e
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-05-22 03:36:08 -0500

    Images for each model type has been uploaded and this data has been added to the inventory file.

---
+++ public/images/models/camper/Wilderness/Facebook_files/iQyo7vJA5va.js
@@ -0,0 +1,5 @@
+;/*FB_PKG_DELIM*/
+
+__d("CometIsAdaptiveUFIEnabled.relayprovider",["qex"],(function(a,b,c,d,e,f,g){"use strict";a={get:function(){var a;return(a=c("qex")._("301"))!=null?a:!1}};g["default"]=a}),98);
+__d("MarketplacePDPContainerQuery_facebookRelayOperation",[],(function(a,b,c,d,e,f){e.exports="5986172354839731"}),null);
+__d("MarketplacePDPRightColumnAdsQuery_facebookRelayOperation",[],(function(a,b,c,d,e,f){e.exports="7549915858405373"}),null);
--- src/Component/Product/Inventory.js
+++ src/Component/Product/Inventory.js
@@ -2,41 +2,85 @@
 const inventory = [
     // POP UPS 
     { 
-        id: 1, 
+        id: 1,
+        year: 2020, 
         name: 'Pop Camper', 
         type: 'Popup', 
         price: 15000,
         sleeps: 5, 
-        image: '/images/rv1.jpg' 
+        image: '/images/rv1.jpg',
     },
     // TRAVEL TRAILERS 
     { 
-        id: 2, 
+        id: 2,
+        year: 2020, 
         name: 'Travel Trailer', 
         type: 'Trailer', 
         price: 20000,
         sleeps: 5, 
-        image: '/images/rv2.jpg' 
+        image: '/images/rv2.jpg',
     },
     // BUMPER PULLS 
     { 
-        id: 3, 
+        id: 3,
+        year: 2020, 
         name: 'Fifth Wheel', 
         type: 'Fifth', 
         price: 35000,
         sleeps: 5, 
-        image: '/images/rv3.jpg' 
+        image: '/images/rv3.jpg',
     },
     // FIFTH WHEELS 
     { 
-        id: 4, 
+        id: 4,
+        year: 2020, 
         name: 'Toy Hauler', 
         type: 'ToyHauler', 
         price: 45000,
         sleeps: 5, 
-        image: '/images/rv4.jpg' 
+        image: '/images/rv4.jpg',
     },
     // Add more RVs
+
+    { 
+        id: 5,
+        year: 2020, 
+        name: 'Flagstaff', 
+        type: 'Trailer', 
+        price: 7300,
+        sleeps: 5, 
+        image: '/images/rv2.jpg',
+    },
+
+    { 
+        id: 6,
+        year: 2020, 
+        name: 'Laredo', 
+        type: 'Fifth', 
+        price: 10300,
+        sleeps: 5, 
+        image: '/images/rv2.jpg',
+    },
+
+    { 
+        id: 7,
+        year: 2016, 
+        name: 'Outback', 
+        type: 'Travel', 
+        price: 14900,
+        sleeps: 5, 
+        image: '/images/rv2.jpg',
+    },
+
+    { 
+        id: 8,
+        year: 2007, 
+        name: 'Wilderness', 
+        type: 'Camper', 
+        price: 8900,
+        sleeps: 5, 
+        image: '/images/rv2.jpg',
+    },
   ];
   
 export default inventory;
